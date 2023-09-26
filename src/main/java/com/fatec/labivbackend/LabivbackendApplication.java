997ae406207d7bb30ddbba526a6003471982a148
William Antoniazzi <william.antoniazzi@fatec.sp.gov.br>
2023-09-26 20:41:36 -0300

    Exemplo prova LabIV Backend

---
+++ src/main/java/com/fatec/labivbackend/LabivbackendApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.labivbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabivbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabivbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/labivbackend/LabivbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.labivbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabivbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
